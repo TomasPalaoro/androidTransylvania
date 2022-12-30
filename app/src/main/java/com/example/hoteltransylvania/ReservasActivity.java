b82568fa884a43401fc7066eab54f19fce3257d1
Tomas <tomaspalaoro@elcampico.org>
2022-12-30 19:13:48 +0100

    numero de adultos y menores

--- app/src/main/java/com/example/hoteltransylvania/ReservasActivity.java
+++ app/src/main/java/com/example/hoteltransylvania/ReservasActivity.java
@@ -7,29 +7,39 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ReservasActivity extends AppCompatActivity {
-    EditText pickerEntrada, pickerSalida;
+
+    static boolean entradaIntroducida, salidaIntroducida;
+    EditText pickerEntrada, pickerSalida, numeroAdultos, numeroMenores;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservas);
 
         pickerEntrada = (EditText) findViewById(R.id.datePickerEntrada);
+        entradaIntroducida = false;
         pickerSalida = (EditText) findViewById(R.id.datePickerSalida);
+        salidaIntroducida = false;
 
         pickerEntrada.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mostrarSeleccionFecha(pickerEntrada);
+                entradaIntroducida = true;
             }
         });
         pickerSalida.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mostrarSeleccionFecha(pickerSalida);
+                salidaIntroducida = true;
             }
         });
+
+        numeroAdultos = (EditText) findViewById(R.id.numeroAdultos);
+        numeroMenores = (EditText) findViewById(R.id.numeroMenores);
     }
 
     private void mostrarSeleccionFecha(final EditText editText) {
@@ -43,5 +53,22 @@ public class ReservasActivity extends AppCompatActivity {
         });
 
         datePickerFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void buscarPulsado(View view) {
+        if (entradaIntroducida && salidaIntroducida){
+
+            int adultos = Integer.parseInt(numeroAdultos.getText().toString());
+            int menores = Integer.parseInt(numeroMenores.getText().toString());
+            String fechaEntrada = pickerEntrada.getText().toString();
+            String fechaSalida = pickerSalida.getText().toString();
+
+            if (adultos<1) Toast.makeText(this, "Debe ir al menos un adulto", Toast.LENGTH_SHORT).show();
+            else Toast.makeText(this, adultos+" adultos "+menores+" niños "+fechaEntrada+" - "+fechaSalida, Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "Introduce una fecha", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
