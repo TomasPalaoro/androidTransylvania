c9227cdeecfe300877230bd84b22ee2246ab3b89
Tomas <tomaspalaoro@elcampico.org>
2022-12-30 16:12:21 +0100

    datepicker guarda fecha

--- app/src/main/java/com/example/hoteltransylvania/DatePickerFragment.java
+++ app/src/main/java/com/example/hoteltransylvania/DatePickerFragment.java
@@ -4,27 +4,32 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.icu.util.Calendar;
 import android.os.Bundle;
-import android.widget.DatePicker;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
-public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+public class DatePickerFragment extends DialogFragment{
+    //CREAR LISTENER
+    private DatePickerDialog.OnDateSetListener listener;
 
+    public static DatePickerFragment newInstance(DatePickerDialog.OnDateSetListener listener) {
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setListener(listener);
+        return fragment;
+    }
+
+    public void setListener(DatePickerDialog.OnDateSetListener listener) {
+        this.listener = listener;
+    }
+    /////////////////
     @Override
+    @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Usar la fecha actual como default
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
 
-        // nueva instancia de datepickerdialog
-        return new DatePickerDialog(getActivity(), this, year, month, day);
-    }
-
-    @Override
-    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-        Toast.makeText(getContext(), ""+i+i1+i2, Toast.LENGTH_SHORT).show();
+        return new DatePickerDialog(getActivity(), listener, year, month, day);
     }
 }
--- app/src/main/java/com/example/hoteltransylvania/ReservasActivity.java
+++ app/src/main/java/com/example/hoteltransylvania/ReservasActivity.java
@@ -2,28 +2,38 @@ package com.example.hoteltransylvania;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 public class ReservasActivity extends AppCompatActivity {
-
+    EditText etdatePicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservas);
 
-        EditText etdatePicker = (EditText) findViewById(R.id.editTextDatePicker);
+        etdatePicker = (EditText) findViewById(R.id.editTextDatePicker);
         etdatePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showDatePickerDialog();
+                mostrarSeleccionFecha();
             }
         });
     }
 
-    private void showDatePickerDialog() {
-        DatePickerFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "datePicker");
+    private void mostrarSeleccionFecha() {
+        DatePickerFragment datePickerFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // +1 porque enero es 0
+                final String fechaSeleccionada = day + " / " + (month+1) + " / " + year;
+                etdatePicker.setText(fechaSeleccionada);
+            }
+        });
+
+        datePickerFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
