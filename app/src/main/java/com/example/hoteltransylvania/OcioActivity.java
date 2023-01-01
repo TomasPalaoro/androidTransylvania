08512d05766c71eff9d9abf9b72e6ac16ed94c52
Tomas <tomaspalaoro@elcampico.org>
2023-01-01 13:13:58 +0100

    colores y fondo

---
+++ app/src/main/java/com/example/hoteltransylvania/OcioActivity.java
@@ -0,0 +1,14 @@
+package com.example.hoteltransylvania;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OcioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ocio);
+    }
+}
