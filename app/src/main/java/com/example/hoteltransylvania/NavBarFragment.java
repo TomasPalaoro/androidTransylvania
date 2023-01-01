912f0f8b7060ae0c4b93121fde6ba870d75bc2bd
Tomas <tomaspalaoro@elcampico.org>
2023-01-01 17:19:09 +0100

    intents navbar

--- app/src/main/java/com/example/hoteltransylvania/NavBarFragment.java
+++ app/src/main/java/com/example/hoteltransylvania/NavBarFragment.java
@@ -1,5 +1,6 @@
 package com.example.hoteltransylvania;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -24,18 +25,24 @@ public class NavBarFragment extends Fragment {
         bottomNav = vistaFragment.findViewById(R.id.bottomNav);
 
         bottomNav.setOnItemSelectedListener(item -> {
+            Class<?> activityDestino = null;
             switch (item.getItemId()){
                 case R.id.home:
-                    Toast.makeText(getActivity(), "Home", Toast.LENGTH_SHORT).show();
+                    activityDestino = ReservasActivity.class;
                     break;
                 case R.id.perfil:
-                    Toast.makeText(getActivity(), "perfil", Toast.LENGTH_SHORT).show();
+                    activityDestino = PerfilActivity.class;
                     break;
-                case R.id.chat:
-                    Toast.makeText(getActivity(), "chat", Toast.LENGTH_SHORT).show();
-                    break;
+                /*case R.id.chat:
+                    activityDestino = MainActivity.class;
+                    break;*/
                 default:
+                    activityDestino = MainActivity.class;
                     break;
+            }
+            if (!getActivity().getClass().equals(activityDestino)){
+                Intent i = new Intent(getActivity().getApplicationContext(), activityDestino);
+                startActivity(i);
             }
             return true;
         });
